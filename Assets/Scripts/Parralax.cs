5015d036c9002e32f3c8746ba7887730a49cb809
Max Maley <1030163@student.sae.edu.au>
2023-07-29 17:46:23 +0800

    Scene 2 added and decorated woo

--- Assets/Scripts/Parralax.cs
+++ Assets/Scripts/Parralax.cs
@@ -17,7 +17,7 @@ public class Parralax : MonoBehaviour
     private Vector2 startPosition;
     public GameObject ForeGround, MidGround, BackGround, ReallyFarBackground;
     private Vector2 ForeGroundf, MidGroundf, BackGroundf, ReallyFarBackgroundf;
-
+    public float foreGroundMult, backGroundMult, midGroundMult;
 
 
     public void Parralaxer()
@@ -27,9 +27,9 @@ public class Parralax : MonoBehaviour
 
         Vector2 Parralax = startPosition - new Vector2(transform.position.x, transform.position.y);
 
-        ForeGround.transform.position = Parralax * new Vector2(0.05f,0f) + ForeGroundf;
-        MidGround.transform.position = Parralax * new Vector2(0.02f, 0f) + MidGroundf;
-        BackGround.transform.position = Parralax * new Vector2(0.01f, 0f) + BackGroundf;
+        ForeGround.transform.position = foreGroundMult * Parralax * new Vector2(0.05f,0.01f) + ForeGroundf;
+        MidGround.transform.position = midGroundMult * Parralax * new Vector2(0.02f, 0.01f) + MidGroundf;
+        BackGround.transform.position = backGroundMult * Parralax * new Vector2(0.01f, 0.01f) + BackGroundf;
     }
 
     // Update is called once per frame
